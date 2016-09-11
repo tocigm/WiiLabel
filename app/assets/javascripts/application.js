9fe4ee48ad1b9e15cafcec42214f0eb4c2b726cc
duyvk <duyvk142@gmail.com>
2016-09-11 18:06:15 +0200

    done with comments

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sass/assets/javascripts/bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,18 @@ Bundler.require(*Rails.groups)
 
 module WiiLabel
   class Application < Rails::Application
+
+    # Bower asset paths
+    root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
+      config.sass.load_paths << bower_path
+      config.assets.paths << bower_path
+    end
+    # Precompile Bootstrap fonts
+    config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
+    # Minimum Sass number precision required by bootstrap-sass
+    ::Sass::Script::Value::Number.precision = [8, ::Sass::Script::Value::Number.precision].max
+
+
   	# config.generators do |g|
   	# 	g.orm :mongoid
     # Settings in config/environments/* take precedence over those specified here.
