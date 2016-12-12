6995faf80469781f300fd6edc863efa46ed44f40
kduy <duyvk142@gmail.com>
2016-12-12 12:17:20 +0100

    Done

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,15 +2,16 @@ class Item
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
   field :category, type: String
+  field :name, type: String
   field :description, type: String
-  field :tags, type: String
+  field :currency, type: String
   field :price, type: String
-  field :title, type: String
   field :item_id, type: String
   field :image_urls, type: String
   field :source, type: String
   field :page_id, type: String
   field :page_url, type: String
+  field :tags, type: String
   field :status, type:String, default: "unchecked"
   embeds_many :images
 
